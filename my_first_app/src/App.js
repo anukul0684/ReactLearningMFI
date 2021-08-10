2bdf81772605c8a5f535ecebe691723f08a3f793
akulshrestha06 <akulshrestha@marketforce.com>
2021-08-10 16:32:52 -0500

    added Card.js and Card.css and imported in App.js file

--- my_first_app/src/App.js
+++ my_first_app/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Card from './Card';
+import './Card.css';
+
 function App() {
   return (
     <div className="App">
+      <Card />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>My First React App!!</h1>
