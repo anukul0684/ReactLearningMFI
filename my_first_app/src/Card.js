9d4cad4d14fceb2c37b1eba6329e19e23ca17bd9
akulshrestha06 <akulshrestha@marketforce.com>
2021-08-10 17:13:41 -0500

    custom React Component hard code and styling complete

--- my_first_app/src/Card.js
+++ my_first_app/src/Card.js
@@ -4,8 +4,19 @@ class Card extends Component {
     render() {
         return (
             <React.Fragment>
-                <h1>Hello World!!</h1>
-                <h2>Hello Again.</h2>
+                <section class="card-container">
+                    <header>
+                        <span initials="AK"></span>
+                        <h2>Anu Kulshrestha</h2>
+                    </header>
+                    <main>
+                        <ul>
+                            <li><span>Birthday: </span>Feb 06, 1984</li>
+                            <li><span>Address: </span>2945 Pembina Highway</li>
+                            <li><span>Phone No.: </span>204-599-8236</li>
+                        </ul>
+                    </main>
+                </section>
             </React.Fragment>
         );
     }
